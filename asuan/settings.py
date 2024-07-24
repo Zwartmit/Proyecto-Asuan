50d911823cb7d8c0197e5c1b5f1393fb534a4447
Zwartmit <brandonurbanobuz@gmail.com>
2024-07-24 13:16:13 -0500

    Unión de implementación de restablecimiento de contraseña y actualización de diseño

--- asuan/settings.py
+++ asuan/settings.py
@@ -135,3 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'davidcubides05@gmail.com' 
+EMAIL_HOST_PASSWORD = 'ndvz xysg blle oytt'
