c520783d20e3b39d87eb8d039f1aeb09b4e8ef7e
David <davidcubides05@gmail.com>
2024-07-31 17:49:15 -0500

    se crearon los grupos de administrador y operador y se crearon permisos de usuario para el operador

--- asuan/settings.py
+++ asuan/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'davidcubides05@gmail.com' 
-EMAIL_HOST_PASSWORD = 'ndvz xysg blle oytt'
+EMAIL_HOST_USER = 'asuan.adso@gmail.com' 
+EMAIL_HOST_PASSWORD = 'bskh fdek gqhu eidk'
